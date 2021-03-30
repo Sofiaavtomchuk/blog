f4c66a16a86f3e3bd1df245d81d15e8163609982
hp <sofiia.avtomchuk@oa.edu.ua>
2021-03-30 21:16:34 +0300

    Lab3

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 Route::resource('rest', RestTestController::class)->names('restTest');
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
